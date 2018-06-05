4623eda1a3e39ac6f21f78ecad206cb643d015a9
caio-vitor <caio_vit@yahoo.com.br>
2018-06-05 19:33:23 -0300

    Update Teresa.vue

--- src/components/Teresa.vue
+++ src/components/Teresa.vue
@@ -46,6 +46,7 @@ export default {
 .quem-img {
     background-image: url('../assets/img3934.jpg');
     background-size: cover;
+    background-position: center;
     min-height: 50vh;
 }
 .quem-txt {
